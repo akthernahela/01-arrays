dd804d2f86a6f817617702d44a8ad71d1e4ec233
akthernahela <nahela00@gmail.com>
2025-10-08 18:25:43 +0200

    Fix reversedTeachers  and initialize longNames

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -13,16 +13,28 @@ console.log(teachers);
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers;
-for ( let i =teachers.length - 1; i >= 0; i--){
+for (let i = teachers.length - 1; i >= 0; i--) {
   const teacher = teachers[i];
   console.log(teacher);
 }
+//Creo il ciclo for()
+//Parte dall'ultimo elemento
+//Inverto l'array
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i = 0; i < teachers.length; i++){
+  if ( teachers[i].length >= 5){
+    longNames.push(teachers[i]);
+  }
+}
+console.log(longNames);
+//Faccio un array vuoto nel quale dopo appare i nomi che voglio selezionare
+//Devo dare un istruzione per selezionare la lunghezza degli elementi che voglio 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
